521566291c9865d52baa223faa2b8d4a8c0ae446
nagababu <nag4c1@gmail.com>
2023-02-06 17:48:47 +0530

    updating ./learn-terraform

--- outputs/demo.tf
+++ outputs/demo.tf
@@ -1,3 +1,11 @@
 output "demo" {
   value = "hello nagababu from terraform"
 }
+
+output "updated" {
+  value = "Installed terraform in workstation successfully"
+}
+
+output "demo1" {
+  value = "hello1"
+}
