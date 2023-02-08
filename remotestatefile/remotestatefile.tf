0f8910ef753a9abaff6b6affb7dc59339485b322
nagababu <nag4c1@gmail.com>
2023-02-08 15:11:56 +0530

    updating ./learn-terraform

--- remotestatefile/remotestatefile.tf
+++ remotestatefile/remotestatefile.tf
@@ -11,6 +11,7 @@ data "aws_ami" "centos8" {
 resource "aws_instance" "web" {
   ami = data.aws_ami.centos8.id
   instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
   tags = {
     Name = "Ec2-test"
   }
@@ -20,5 +21,25 @@ terraform {
     bucket = "terraform-nag-b70"
     key    = "remotestatefile/terraform.tfstatefile"
     region = "us-east-1"
+  }
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow tls inbound traffic"
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "allow_tls"
   }
 }
