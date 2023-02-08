c543286a78aa1f8ef8c6a47f73485bd2060deb34
nagababu <nag4c1@gmail.com>
2023-02-08 13:47:40 +0530

    updating ./learn-terraform

--- data-sources/data.tf
+++ data-sources/data.tf
@@ -4,22 +4,25 @@ data "aws_ami" "example" {
   owners           = ["973714476881"]
 
 }
-
-output "ami" {
-  value = data.aws_ami.example
-}
-data "aws_instance" "foo" {
-  instance_id = "i-092cd4f595a2d424d"
+output "id" {
+  value = "data.aws_ami.example.id"
 }
 
-output "instance_public_ip_address" {
-    value = data.aws_instance.foo.public_ip
-}
-
-data "aws_instance"  "ram" {
-  instance_id = "i-092cd4f595a2d424d"
-}
-
-output "instance_private_ip_address" {
-  value = data.aws_instance.ram.private_ip
-}
+#output "ami" {
+#  value = data.aws_ami.example
+#}
+#data "aws_instance" "foo" {
+#  instance_id = "i-092cd4f595a2d424d"
+#}
+#
+#output "instance_public_ip_address" {
+#    value = data.aws_instance.foo.public_ip
+#}
+#
+#data "aws_instance"  "ram" {
+#  instance_id = "i-092cd4f595a2d424d"
+#}
+#
+#output "instance_private_ip_address" {
+#  value = data.aws_instance.ram.private_ip
+#}
