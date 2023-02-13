76d75164176ce34e2aaf50431a92300d00871296
nagababu <nag4c1@gmail.com>
2023-02-13 19:34:23 +0530

    updating ./learn-terraform

---
+++ loops/for-each-module/main.tf
@@ -0,0 +1,21 @@
+variable "components" {
+  default = {
+    cart = {
+      name    = "cart"
+      instance_type = "t3.micro"
+    }
+    catalogue = {
+      name = "catalogue"
+      instance_type = "t3.small"
+    }
+  }
+}
+
+
+
+module "ec2" {
+  source = "./module"
+  for_each = var.components
+  instance_type = each.value.instance_type
+  name = each.value.name
+}
