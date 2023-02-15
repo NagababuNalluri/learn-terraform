79ca9ca0f60eaa3e823f3bc0eed168809fb4c218
nagababu <nag4c1@gmail.com>
2023-02-15 18:01:31 +0530

    updating ./learn-terraform

---
+++ locals/locals1/main.tf
@@ -0,0 +1,24 @@
+variable "components" {
+  default = {
+    cart = {
+      name = "cart"
+      instance_type = "t3.micro"
+    }
+    catalogue = {
+      name = "catalogue"
+      instance_type = "t3.small"
+    }
+  }
+}
+
+locals{
+  name          = {for k, v in var.components: k=> v.name}
+  instance_type = {for k, v in var.components : k=> v.instance_type}
+}
+
+output "name" {
+  value = local.name
+}
+output "instance_type" {
+  value = local.instance_type
+}
