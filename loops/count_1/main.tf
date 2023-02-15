1b999fc461654a10b22598c9a8ae550aa2c695d7
nagababu <nag4c1@gmail.com>
2023-02-15 12:55:59 +0530

    updating ./learn-terraform

--- loops/count_1/main.tf
+++ loops/count_1/main.tf
@@ -1,13 +1,13 @@
 resource "aws_instance" "web" {
   count = length(var.components)
-  ami = data.aws_ami.id
+  ami = data.aws_ami.centos8.id
   instance_type = "t3.micro"
   tags = {
     Name = var.components[count.index]
   }
 }
 
-data "aws_ami" "id" {
+data "aws_ami" "centos8" {
   most_recent      = true
   name_regex       = "Centos-8-DevOps-Practice"
   owners           = ["973714476881"]
