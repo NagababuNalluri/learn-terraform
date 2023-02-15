c31a7213d213cb3f4ad50881090f21f83f10539e
nagababu <nag4c1@gmail.com>
2023-02-15 16:38:50 +0530

    updating ./learn-terraform

--- loops/for-each-module1/ec2.tf
+++ loops/for-each-module1/ec2.tf
@@ -1,11 +1,27 @@
 resource "aws_instance" "web" {
+  for_each = var.components
   ami = data.aws_ami.centos.id
-  instance_type = "t3.micro"
+  instance_type = each.value.instance_type
   tags = {
-    Name = "Testone"
+    Name = each.value.name
   }
 }
 data "aws_ami" "centos" {
   most_recent = true
   owners = ["973714476881"]
+}
+variable "components" {
+  default = {
+    cart = {
+      name = "cart"
+      instance_type = "t3.micro"
+    }
+    catalogue = {
+      name = "catalogue"
+      instance_type = "t3.small"
+    }
+  }
+}
+output "publicip" {
+  value = {for k,v in aws_instance.web : k => v.public_ip}
 }
