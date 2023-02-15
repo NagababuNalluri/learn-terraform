ad5ea76b19b623690812c45b297a7cb5c33c4d4b
nagababu <nag4c1@gmail.com>
2023-02-15 12:45:09 +0530

    updating ./learn-terraform

---
+++ loops/count_1/main.tf
@@ -0,0 +1,18 @@
+resource "aws_instance" "web" {
+  count = 2
+  ami = data.aws_ami.example
+  instance_type = "t3.micro"
+  tags = {
+    Name = "var.components[count:index]"
+  }
+}
+
+data "aws_ami" "example" {
+  most_recent      = true
+  name_regex       = "Centos-8-DevOps-Practice"
+  owners           = ["973714476881"]
+
+}
+variable "components" {
+  default = ["cart","frontend"]
+}
